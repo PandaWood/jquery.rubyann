8a6c81279be9e51d68fda50d94775024336f72a6
Peter van der Woude <pandawoude@gmail.com>
2011-01-01 08:59:44 +1100

    add option to change default delimiter characters '{}'

--- jquery.rubyann.coffee
+++ jquery.rubyann.coffee
@@ -9,18 +9,28 @@ baseXml =
 </ruby>'
 
 $.fn.extend({
-	rubyann: ->
+	rubyann: (options) ->
+		defaults =
+			delimiters: '{}'
+
+		options = $.extend defaults, options
+		return if options.length is not 2
+		delimitStart = "\\#{options.delimiters[0]}"
+		delimitEnd = "\\#{options.delimiters[1]}"
+
 		@each ->
 			storyText = $(@).text()
-			matches = storyText.match(/\{.*?,.*?\}/g)
+			matches = storyText.match ///
+					#{delimitStart}.*?,.*?#{delimitEnd}
+				///g
 			return @ if matches is null
 
 			$.each matches, (index,value) ->
 				split = value.split ','
 				return if split.length isnt 2
 
-				rbase = split[0].replace '{',''
-				rtext = split[1].replace '}',''
+				rbase = split[0].replace options.delimiters[0],''
+				rtext = split[1].replace options.delimiters[1],''
 				return if rbase.length is 0 or rtext.length is 0
 				
 				rxml = baseXml.replace('{rbase}', rbase)
--- jquery.rubyann.js
+++ jquery.rubyann.js
@@ -8,11 +8,21 @@
 	<rp>)</rp>\
 </ruby>';
   $.fn.extend({
-    rubyann: function() {
+    rubyann: function(options) {
+      var defaults, delimitEnd, delimitStart;
+      defaults = {
+        delimiters: '{}'
+      };
+      options = $.extend(defaults, options);
+      if (options.length === !2) {
+        return;
+      }
+      delimitStart = "\\" + options.delimiters[0];
+      delimitEnd = "\\" + options.delimiters[1];
       return this.each(function() {
         var matches, storyText;
         storyText = $(this).text();
-        matches = storyText.match(/\{.*?,.*?\}/g);
+        matches = storyText.match(RegExp("" + delimitStart + ".*?,.*?" + delimitEnd, "g"));
         if (matches === null) {
           return this;
         }
@@ -22,8 +32,8 @@
           if (split.length !== 2) {
             return;
           }
-          rbase = split[0].replace('{', '');
-          rtext = split[1].replace('}', '');
+          rbase = split[0].replace(options.delimiters[0], '');
+          rtext = split[1].replace(options.delimiters[1], '');
           if (rbase.length === 0 || rtext.length === 0) {
             return;
           }
