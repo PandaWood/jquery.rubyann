1ca816747b183b7ce5f06fb2eb07b00caaf8212a
Peter van der Woude <pandawoude@gmail.com>
2017-10-20 22:02:47 +1100

    small non-code updates
    npm dependencies and readme

--- gulpfile.js
+++ gulpfile.js
@@ -8,21 +8,21 @@ var uglify = require('gulp-uglify')
 // transpile coffee src
 gulp.task('coffee', function() {
 
-	gulp.src('./src/*.coffee')
+	gulp.src('src/*.coffee')
 		.pipe(coffee())
 		.pipe(uglify())
-		.pipe(gulp.dest('./dist'))
+		.pipe(gulp.dest('dist'))
 
 })
 
 // transpile test coffee and run qunit tests
 gulp.task('test', function() {
 
-	gulp.src('./tests/*.coffee')
+	gulp.src('tests/*.coffee')
 		.pipe(coffee())
 		.pipe(gulp.dest('./js'))
 
-	gulp.src('./tests/rubyann_tests.html')
+	gulp.src('tests/rubyann_tests.html')
 		.pipe(qunit())
 
 })
