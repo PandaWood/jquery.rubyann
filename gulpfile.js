ff00ccefcd2e74627135b6fc8d5d986104d00305
Peter van der Woude <pandawoude@gmail.com>
2018-05-08 18:46:38 +1000

    add typescript typings and set main in package.json

--- gulpfile.js
+++ gulpfile.js
@@ -7,24 +7,20 @@ var uglify = require('gulp-uglify')
 
 // transpile coffee src
 gulp.task('coffee', function() {
-
 	gulp.src('src/*.coffee')
 		.pipe(coffee())
 		.pipe(uglify())
 		.pipe(gulp.dest('dist'))
-
 })
 
 // transpile test coffee and run qunit tests
 gulp.task('test', function() {
-
 	gulp.src('tests/*.coffee')
 		.pipe(coffee())
 		.pipe(gulp.dest('./js'))
 
 	gulp.src('tests/rubyann_tests.html')
 		.pipe(qunit())
-
 })
 
 gulp.task('default', ['coffee', 'test'])
---
+++ ts/jquery,rubyann.d.ts
@@ -0,0 +1,13 @@
+interface RubyAnnOptions {
+	/**
+	 * The closing and opening delimeter characters as a single string eg "[]" or "@@"
+	 * Delimiters are used around each word/script in the ruby syntax - the default is "{}"
+	 * eg "The word for 'japanese language' is {日,に}{本,ほん}{語,ご}"
+	 */
+	delimiters: string
+}
+
+// noinspection JSUnusedGlobalSymbols
+interface JQuery {
+	rubyann(options?: RubyAnnOptions): JQuery
+}
