793db12a71147009296018b2b3415ab137682c90
Peter van der Woude <pandawoude@gmail.com>
2018-06-30 13:46:19 +1000

    update typescript typings to use index.d.ts filename

--- ts/index.d.ts
+++ ts/index.d.ts
@@ -8,7 +8,7 @@
 
 interface RubyAnnOptions {
 	/**
-	 * The closing and opening delimeter characters as a single string eg "[]" or "@@" - default is "{}"
+	 * The closing and opening delimiter characters as a single string eg "[]" or "@@" - default is "{}"
 	 * These delimiters are used around each word in our custom ruby syntax
 	 * eg "The word for 'japanese language' is {日,に}{本,ほん}{語,ご}"
 	 */
